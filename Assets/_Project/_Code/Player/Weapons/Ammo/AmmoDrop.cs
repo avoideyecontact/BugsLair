b5f658a147f447341cd253215e77865ea3ee3f57
Vladimir Elshansky <vendetta_gt@mail.ru>
2025-10-31 01:29:47 +0500

    Ammo Drop

--- Assets/_Project/_Code/Player/Weapons/Ammo/AmmoDrop.cs
+++ Assets/_Project/_Code/Player/Weapons/Ammo/AmmoDrop.cs
@@ -1,3 +1,5 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 
 public class AmmoDrop : MonoBehaviour
@@ -11,6 +13,25 @@ public class AmmoDrop : MonoBehaviour
         {
             other.GetComponentInChildren<PlayerWeaponary>().AddAmmo(_ammoType, value);
             Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        AnimationTask().Forget();
+    }
+
+    private async UniTask AnimationTask()
+    {
+        var ct = this.GetCancellationTokenOnDestroy();
+        Transform child = transform.GetChild(0);
+
+        while (!ct.IsCancellationRequested)
+        {
+            var rotateTask = child.DORotate(new Vector3(0, 180, 0), 4f).SetEase(Ease.Linear).SetRelative().WithCancellation(ct);
+            await child.DOMoveY(0.5f, 2f).SetEase(Ease.InOutSine).SetRelative().WithCancellation(ct);
+            await child.DOMoveY(-0.5f, 2f).SetEase(Ease.InOutSine).SetRelative().WithCancellation(ct);
+            await rotateTask;
         }
     }
 }
