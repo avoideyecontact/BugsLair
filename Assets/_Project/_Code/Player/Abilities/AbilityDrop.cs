16f9287301b8993494bd8fab387860b35e69aae0
Vladimir Elshansky <vendetta_gt@mail.ru>
2025-11-09 15:14:58 +0500

    Abilities models

--- Assets/_Project/_Code/Player/Abilities/AbilityDrop.cs
+++ Assets/_Project/_Code/Player/Abilities/AbilityDrop.cs
@@ -15,14 +15,17 @@ public class AbilityDrop : MonoBehaviour
     private async UniTask AnimationTask()
     {
         var ct = this.GetCancellationTokenOnDestroy();
-        Transform child = transform.GetChild(0);
+        Transform child0 = transform.GetChild(0);
+        Transform child1 = transform.GetChild(1);
 
         while (!ct.IsCancellationRequested)
         {
-            var rotateTask = child.DORotate(new Vector3(0, 180, 0), 4f).SetEase(Ease.Linear).SetRelative().WithCancellation(ct);
-            await child.DOMoveY(0.25f, 2f).SetEase(Ease.InOutSine).SetRelative().WithCancellation(ct);
-            await child.DOMoveY(-0.25f, 2f).SetEase(Ease.InOutSine).SetRelative().WithCancellation(ct);
-            await rotateTask;
+            var rotateTask1 = child0.DORotate(new Vector3(0, 180, 0), 4f).SetEase(Ease.Linear).SetRelative().WithCancellation(ct);
+            var rotateTask2 = child1.DORotate(new Vector3(0, -180, 0), 4f).SetEase(Ease.Linear).SetRelative().WithCancellation(ct);
+            await child1.DOMoveY(0.5f, 2f).SetEase(Ease.InOutSine).SetRelative().WithCancellation(ct);
+            await child1.DOMoveY(-0.5f, 2f).SetEase(Ease.InOutSine).SetRelative().WithCancellation(ct);
+            await rotateTask1;
+            await rotateTask2;
         }
     }
 }
